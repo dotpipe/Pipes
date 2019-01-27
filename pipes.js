dc0c91948087f6a515988a916f848e5047679c80
swatchphp <tcp@null.net>
2019-01-27 13:20:08 -0500

    Update pipes.js

--- pipes.js
+++ pipes.js
@@ -13,27 +13,29 @@ window.addEventListener("click", function(ev) {
 	var elem = document.getElementById(ev.target.id);
 	if (elem === null || elem === undefined)
 		return;
-	if (!elem.hasAttribute("thru-pipe")) {
-		if (elem.hasAttribute("to-pipe") && elem.getAttribute("to-pipe") !== "")
-			window.location.replace(elem.getAttribute("to-pipe"));
-		return;
-	}
-	if (elem.hasAttribute("href") && elem.getAttribute("href") !== undefined && elem.getAttribute("href") !== null)
+	if (elem.hasAttribute("href"))
 		window.location.replace(elem.getAttribute("href"));
 	var return_method = "";
 	var elem_values = document.getElementsByClassName("data-pipe");
 	var elem_qstring = "";
-	for (var i = 0 ; i < elem_values.length ; i++)
-		elem_qstring = elem_qstring + elem_values[i].name + "=" + elem_values[i].value + "&";
+	for (var i = 0 ; i < elem_values.length ; i++) {
+		var val = "";
+		elem_qstring = elem_qstring + elem_values[i].name + "=" + elem_values[i].getAttribute("value") + "&";
+	}
 	
 	if (elem_qstring[elem_qstring] === "&")
-		elem_qstring = elem_qstring.substring(0, elem_qstring.length() - 1);
+		elem_qstring = elem_qstring.substring(0, elem_qstring.length - 1);
 
+	if (!elem.hasAttribute("thru-pipe")) {
+		if (elem.hasAttribute("to-pipe") && elem.getAttribute("to-pipe") !== "")
+			window.location.href = elem.getAttribute("to-pipe") + "?" +  elem_qstring.substring(0, elem_qstring.length - 1);
+		return;
+	}
 	(!elem.hasAttribute("method")) ? method_thru = "GET" : method_thru = elem.getAttribute("method");
 	(!elem.hasAttribute("mode")) ? mode_thru = "no-cors" : mode_thru = elem.getAttribute("mode");
 	(!elem.hasAttribute("cache")) ? cache_thru = "no-cache" : cache_thru = elem.getAttribute("cache");
 	(!elem.hasAttribute("credentials")) ? cred_thru = "same-origin" : cred_thru = elem.getAttribute("cred");
-	(!elem.hasAttribute("headers")) ? content_thru = '"Access-Control-Allow-Origin":"*"' : content_thru = elem.getAttribute("content");
+	(!elem.hasAttribute("headers")) ? content_thru = '{"Access-Control-Allow-Origin":"*","Content-Type":"text/html"}' : content_thru = elem.getAttribute("content");
 	(!elem.hasAttribute("redirect")) ? redirect_thru = "manual" : redirect_thru = elem.getAttribute("redirect");
 	(!elem.hasAttribute("referrer")) ? refer_thru = "client" : refer_thru = elem.getAttribute("referrer");
 
@@ -47,10 +49,10 @@ window.addEventListener("click", function(ev) {
 	opts.push("redirect", redirect_thru); // manual, *follow, error
 	opts.push("referrer", refer_thru); // no-referrer, *client
 	if (content_thru == "application/json") {
-		opts.push("body", JSON.stringify(elem_qstring));
+		opts.push('body', JSON.stringify(elem_qstring));
 	}
 	else {
-		opts.push("body", elem_qstring); // body data type must match "Content-Type" header
+		opts.push('body', elem_qstring); // body data type must match "Content-Type" header
 	}
 	const abort_ctrl = new AbortController();
 	const signal = abort_ctrl.signal;
@@ -59,5 +61,5 @@ window.addEventListener("click", function(ev) {
 	setTimeout(() => abort_ctrl.abort(), 10 * 1000);
 	fetch(opts_req, opts);
 	if (elem.hasAttribute("to-pipe"))
-		window.location.replace(elem.getAttribute("to-pipe"));
+		window.location.href = elem.getAttribute("to-pipe");
 });
