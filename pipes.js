fe7beae2598a8434b6bfbebe84c74b7b8edf2026
swatchphp <tcp@null.net>
2019-01-27 10:52:31 -0500

    Add files via upload

--- pipes.js
+++ pipes.js
@@ -10,13 +10,15 @@ window.addEventListener("click", function(ev) {
 	var refer_thru = "";
 	var pipe_to = "";
 	var return_method = "";
-	var elem = document.getElementById(ev.target.id);
-	if (elem.getAttribute("thru-pipe") == undefined || elem.getAttribute("thru-pipe") == null) {
-		if (elem.getAttribute("to-pipe") !== undefined && elem.getAttribute("thru-pipe") !== null)
+	var elem = ev.target.id;
+	if (elem === null || elem === undefined)
+		return;
+	if (!elem.hasAttribute("thru-pipe") || (elem.getAttribute("thru-pipe") == undefined || elem.getAttribute("thru-pipe") == null)) {
+		if (elem.hasAttribute("to-pipe") && elem.getAttribute("to-pipe") !== undefined && elem.getAttribute("to-pipe") !== null)
 			window.location.replace(elem.getAttribute("to-pipe"));
 		return;
 	}
-	if (elem.getAttribute("href") !== undefined && elem.getAttribute("href") !== null)
+	if (elem.hasAttribute("href") && elem.getAttribute("href") !== undefined && elem.getAttribute("href") !== null)
 		window.location.replace(elem.getAttribute("href"));
 	var return_method = "";
 	var elem_values = document.getElementsByClassName("data-pipe");
@@ -27,13 +29,13 @@ window.addEventListener("click", function(ev) {
 	if (elem_qstring[elem_qstring] === "&")
 		elem_qstring = elem_qstring.substring(0, elem_qstring.length() - 1);
 
-	(elem.getAttribute("method") == undefined) ? method_thru = "GET" : method_thru = elem.getAttribute("method");
-	(elem.getAttribute("mode") == undefined) ? mode_thru = "cors" : mode_thru = elem.getAttribute("mode");
-	(elem.getAttribute("cache") == undefined) ? cache_thru = "no-cache" : cache_thru = elem.getAttribute("cache");
-	(elem.getAttribute("cred") == undefined) ? cred_thru = "cred" : cred_thru = elem.getAttribute("cred");
-	(elem.getAttribute("headers") == undefined) ? content_thru = "text/html" : content_thru = elem.getAttribute("content");
-	(elem.getAttribute("redirect") == undefined) ? redirect_thru = "follow" : content_thru = elem.getAttribute("redirect");
-	(elem.getAttribute("referrer") == undefined) ? refer_thru = "client" : content_thru = elem.getAttribute("referrer");
+	(!elem.hasAttribute("method")) ? method_thru = "GET" : method_thru = elem.getAttribute("method");
+	(!elem.hasAttribute("mode")) ? mode_thru = "cors" : mode_thru = elem.getAttribute("mode");
+	(!elem.hasAttribute("cache")) ? cache_thru = "no-cache" : cache_thru = elem.getAttribute("cache");
+	(!elem.hasAttribute("cred")) ? cred_thru = "cred" : cred_thru = elem.getAttribute("cred");
+	(!elem.hasAttribute("headers")) ? content_thru = "text/html" : content_thru = elem.getAttribute("content");
+	(!elem.hasAttribute("redirect")) ? redirect_thru = "follow" : content_thru = elem.getAttribute("redirect");
+	(!elem.hasAttribute("thru-pipe")) ? refer_thru = "client" : content_thru = elem.getAttribute("referrer");
 
 	var opts_req = new Request(elem.getAttribute("thru-pipe"));
 	var opts = {
@@ -58,6 +60,6 @@ window.addEventListener("click", function(ev) {
 	setTimeout(() => abort_ctrl.abort(), 10 * 1000);
 	fetch(opts_req, opts);
 	opts.clear();
-	if (elem.getAttribute("to-pipe") !== undefined && elem.getAttribute("to-pipe") !==null)
+	if (elem.hasAttribute("to-pipe"))
 		window.location.replace(elem.getAttribute("to-pipe"));
 });
