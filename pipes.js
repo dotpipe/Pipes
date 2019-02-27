5c77b7d641ccad404e55a5350a6faf28168d27d6
swatchphp <tcp@null.net>
2019-02-27 11:59:54 -0500

    Add files via upload

--- pipes.js
+++ pipes.js
@@ -1,6 +1,3 @@
-// Pipes for PVC & Pirodock
-// on github/swatchphp
-
 ['click', 'touch', 'tap'].forEach(function(e) {
 	window.addEventListener(e, function(ev) {
 	var method_thru = "";
@@ -30,13 +27,12 @@
 
 	//return if non-pipe
 
-
 	// No 'pipe' means it is generic
 	for (var i = 0 ; i < elem_values.length ; i++) {
-		var val = "";
 	//if this is designated as belonging to another pipe, it won't be passed in the url
-		if (!elem_values[i].hasAttribute("pipe") || elem_values[i].getAttribute("pipe") === elem.id)
-			elem_qstring = elem_qstring + elem_values[i].name + "=" + elem_values[i].getAttribute("value") + "&";
+		if (!elem_values[i].hasAttribute("pipe") || elem_values[i].getAttribute("pipe") == elem.id)
+			elem_qstring = elem_qstring + elem_values[i].name + "=" + elem_values[i].value + "&";
+		
 	}
 
 	//strip last & char
@@ -45,11 +41,10 @@
 
 	if (!elem.hasAttribute("thru-pipe")) {
 		if (elem.hasAttribute("to-pipe") && elem.getAttribute("to-pipe") !== "")
-			window.location.href = elem.getAttribute("to-pipe") + "?" +  elem_qstring.substring(0, elem_qstring.length - 1);
+			window.location.href = elem.getAttribute("to-pipe") + "?" + elem_qstring;
 		return;
 	}
 
-	//make up headers and options for fetch call
 	(!elem.hasAttribute("method")) ? method_thru = "GET" : method_thru = elem.getAttribute("method");
 	(!elem.hasAttribute("mode")) ? mode_thru = "no-cors" : mode_thru = elem.getAttribute("mode");
 	(!elem.hasAttribute("cache")) ? cache_thru = "no-cache" : cache_thru = elem.getAttribute("cache");
@@ -58,7 +53,7 @@
 	(!elem.hasAttribute("redirect")) ? redirect_thru = "manual" : redirect_thru = elem.getAttribute("redirect");
 	(!elem.hasAttribute("referrer")) ? refer_thru = "client" : refer_thru = elem.getAttribute("referrer");
 
-	var opts_req = new Request(elem.getAttribute("thru-pipe"));
+	var opts_req = new Request(elem.getAttribute("thru-pipe") + "?" + elem_qstring);
 	opts = new Map();
 	opts.set("method", method_thru); // *GET, POST, PUT, DELETE, etc.
 	opts.set("mode", mode_thru); // no-cors, cors, *same-origin
@@ -77,8 +72,9 @@
 	const signal = abort_ctrl.signal;
 	var target__ = null;
 	if (elem.hasAttribute("out-pipe"))
-		target__ = document.getElementById(elem.hasAttribute("out-pipe"));
+		target__ = document.getElementById(elem.getAttribute("out-pipe"));
 	fetch(opts_req, {signal});
+	
 	setTimeout(() => abort_ctrl.abort(), 3 * 1000);
 	const __grab = async (opts_r, opts_) => {
 		return fetch(opts_r, opts_)
@@ -94,12 +90,8 @@
 	
 	}
 
-	// call-pipe is the callback function for the fetch
-	// You may leave it out, but you won't have a reply
-	// It is recommended to leave at least a file that
-	// you got it. You can do this through setting out-pipe.
-	const getActivity = async (opts_req, opts) => {
-		let g = await __grab(opts_req, opts);
+	const getActivity = async (opts_rq, opts) => {
+		let g = await __grab(opts_rq, opts);
 		if (elem.hasAttribute("call-pipe")) {
 			var t = elem.getAttribute("call-pipe");
 			return (t)(g);
@@ -107,6 +99,7 @@
 		return;
 	}
 	var s = getActivity(opts_req, opts);
+
 	if (elem.hasAttribute("to-pipe"))
 		window.location.href = elem.getAttribute("to-pipe");
 }, false);
