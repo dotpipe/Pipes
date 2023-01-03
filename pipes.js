58ededc3627921dd1a29434406bfecdb7da23ce5
wise-penny <inland14@live.com>
2023-01-02 19:22:11 -0500

    Update pipes.js

--- pipes.js
+++ pipes.js
@@ -44,7 +44,7 @@
       elem.setAttribute("index",index.toString());
       ppfc = document.getElementById(elem.getAttribute("insert").toString());
       
-      if (ppfc.tagName == "video")
+      if (ppfc.tagName == "video" || ppfc.tagName == "img")
           ppfc.setAttribute("src",arr[index]);
       else
       {
