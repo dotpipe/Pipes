e318b6412172a368aac8b9652725b540c97f9ea3
wise-penny <inland14@live.com>
2023-02-16 02:42:19 -0500

    Novelty bug, got it!

--- vidiot.php
+++ vidiot.php
@@ -19,7 +19,6 @@
 
 		public function populate()
 		{
-			$this->path = './';
 			$files = array_diff(scandir($this->path), array('.', '..'));
 			$output = "";
 			foreach ($files as $file)
@@ -27,23 +26,20 @@
 				if ($file == "vidiot.php" || $file == "pipes.js")
 					continue;
 				if (in_array(substr($file,-4),[".ogg",".mp4"]))
-					$output .= ";$file";
-				//$output .= "<input value'$file'>".$file."</input>";
+					$output .= ";$this->path$file";
 			}
 			$out = "<table><tr><td><h2 onclick='pipes(this)' incrIndex headers='mode:no-cors' insert='source' file-order='". substr($output,1) ."' file-index='0'>&lt;</h2></td>";
 			$out .= '<td><p id="ths" onclick="pipes(this)" insert="ths"></p>';
 			$out .= '<div> 
 						<video width="'.$this->width.'" height="'.$this->height.'" loop id="'.$this->id.'" preload="auto" onmouseover="this.pause();" onmouseleave="this.play();" onclick="easy(media, m_seek);">
-							<source id="source" type="video/mp4" src=""/>
+							<source id="source" type="video/mp4" src="" file-index="0"/>
 						</video>
 					</div></td>';
 			$out .= "<td><h2 onclick='pipes(this)' decrIndex headers='mode:no-cors' insert='source' file-order='". substr($output,1) ."' file-index='0'>&gt;</h2></td></tr></table>";
 			echo $out;
 		}
-		//<?=pipes_exec('vidiot.php');
 	}
 	$json = file_get_contents('vidiot.json');
 	$vidiot = new vidiot($json);
-	$vidiot->populate('./');      
-	// "<=pipes_attr('attribute:value');                                                                                                                                                      
+	$vidiot->populate();
 ?>
