691cd25fd3c2d14f9f1163758b34968e46c0fe5d
wise-penny <inland14@live.com>
2023-03-05 17:54:18 -0500

    Add 'init' cammand block from GPG.

--- filter.cryptography.gpg.php
+++ filter.cryptography.gpg.php
@@ -3,6 +3,11 @@ class GPG {
 	private $id;
 	function __invoke(string $command, string ...$params) : mixed//, $param2 = "", $param3 = "")
 	{
+		if ($command === "init")
+		{
+			echo "Cannot invoke object within stdClass";
+			return;
+		}
 		if (!isset($this->id)) { $this->id = gnupg_init(); }
 		$tempFuncCall = 'gnupg_'.$command;
 		try
