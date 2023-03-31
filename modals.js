792c66cdfa67d5f407241202a61495c48708435d
wise-penny <inland14@live.com>
2023-03-31 04:54:55 -0400

    Added GridLock Stable Beta (Modals)

---
+++ modals.js
@@ -0,0 +1,25 @@
+import cssTxtConst from './modals.json' assert { type: 'json' };
+
+var elemBody = document.getElementsByTagName("body")[0]
+
+// Select parent tag
+var elemDIVparent = document.createElement("div");
+
+elemDIVparent.style = cssTxtConst["div"];
+
+var elemText = document.createElement("p");
+elemText.innerText = "Welcome to the PipesJS GridLock"
+
+var elemSubmit = document.createElement("button");
+elemSubmit.innerHTML = "Save"
+elemSubmit.style = cssTxtConst["submit"];
+
+var elemCancel = document.createElement("button");
+elemCancel.innerHTML = "Cancel"
+elemCancel.style = cssTxtConst["cancel"];
+
+elemDIVparent.appendChild(elemText)
+elemDIVparent.appendChild(elemSubmit)
+elemDIVparent.appendChild(elemCancel)
+
+elemBody.append(elemDIVparent)
