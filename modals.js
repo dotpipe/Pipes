545302b8e82f423e68c859e8024b99890767535d
wise-penny <inland14@live.com>
2023-05-30 14:59:26 -0400

    Reup of scrubbed code

--- modals.js
+++ modals.js
@@ -1,71 +1,15 @@
 import jsonElems from '/modals.json' assert { type: 'json' };
 
-var elemBody = document.getElementsByTagName("body")[0]
-var captElemForEach = null;
-var fg = null;
-function forEachElem(elems, fg)
-{
-	Object.entries(elems).forEach((tagName) => {
-		const [key,	 value] = tagName;
-		if (key == "tagname")
-		{
-			try
-			{
-				if ((captElemForEach instanceof Node) && captElemForEach.parentNode != fg)
-					fg.appendChild(captElemForEach);
-			}
-			catch (e)
-			{
-				elemBody.appendChild(fg);
-				fg = document.createElement(value);
-			}
-			captElemForEach = document.createElement(value);
-		}
-		else if (key == "class")
-		{
-			captElemForEach.classList.toggle(value);
-		}
-		else if (key == "textContent")
-		{
-			captElemForEach.innerText = value;
-		}
-		else if (value instanceof Object)
-		{
-			try
-			{
-				console.log(value);
-				const [k,v] = value;
-				forEachElem(v, captElemForEach);
-			}
-			catch (e)
-			{
-				var tempTag = document.createElement(tagName["tagname"]);
-				Object.entries(value).forEach((nest) => {
-					const [k, v] = nest;
-					if (k!="tagname")
-						tempTag.k = v;
-					console.log(k);
-				});
-				captElemForEach.appendChild(tempTag);
-			}
-		}
-		else
-			captElemForEach.setAttribute(key, value);
-	});
-	if (captElemForEach instanceof Node)
-		fg.appendChild(captElemForEach);
-	
-}
+var elemBody = document.getElementsByTagName("body")[0];
 
-function forEachElem1(value, tempTag, j)
+function forEachElem(value, tempTag)
 {
-	j++;
 	var temp = document.createElement(value["tagname"]);
 	Object.entries(value).forEach((nest) => {
 		const [k, v] = nest;
 		
 		if (v instanceof Object)
-			forEachElem1(v, temp, j);
+			forEachElem(v, temp, j);
 		else if (k.toLowerCase() != "tagname" && k.toLowerCase() != "textcontent" && k.toLowerCase() != "innerhtml" && k.toLowerCase() != "innertext")
 		{
 			temp.setAttribute(k,v);
@@ -74,9 +18,8 @@ function forEachElem1(value, tempTag, j)
 		{
 			(k.toLowerCase() == "textcontent") ? temp.textContent = v : (k.toLowerCase() == "innerhtml") ? temp.innerHTML = v : temp.innerText = v;
 		}
-		console.log(tempTag);
 	});
 	tempTag.appendChild(temp);
 }
 
-forEachElem1(jsonElems, elemBody, 0);
+forEachElem(jsonElems, elemBody, 0);
