130b37def8b6a51345fab0e2532ea015e73ae306
wise-penny <inland14@live.com>
2023-08-08 19:43:16 -0400

    Modala Carousel 80% complete

--- pipes.js
+++ pipes.js
@@ -6,36 +6,39 @@
   *  with unlimited inputs/outputs):
   *  Attribute/Tag   |   Use Case
   *  -------------------------------------------------------------
-  *  insert..........= return ajax call to this id
-  *  ajax............= calls and returns the value file's output ex: <pipe ajax="foo.bar" query="key0:value0;" insert="someID">
-  *  query...........= default query string associated with url ex: <anyTag query="key0:value0;key1:value2;" ajax="page.foo">
-  *  <download>......= tag for downloading files ex: <download file="foo.zip" directory="/home/bar/"> (needs ending with slash)
-  *  file............= filename to download
-  *  directory.......= relative or full path of 'file'
-  *  redirect........= "follow" the ajax call in POST or GET mode ex: <pipe ajax="foo.bar" redirect query="key0:value0;" insert="someID">
-  *  <lnk>..........= tag for clickable link <lnk ajax="goinghere.html" query="key0:value0;">
-  *  <pipe>..........= Tag (initializes on DOMContentLoaded Event) ex: <pipe ajax="foo.bar" query="key0:value0;" insert="someID">
-  *  <dyn>...........= Automatic eventListening tag for onclick="pipes(this)" ex: <dyn ajax="foo.bar" query="key0:value0;" insert="someID">
-  *  <timed>.........= Timed result refreshing tags (Keep up-to-date handling on page) ex: <timed ajax="foo.bar" delay="3000" query="key0:value0;" insert="someID">
-  *  delay...........= delay between <timed> tag refreshes (required for <timed> tag) ex: see <timed>
-  *  <carousel>......= Tag to create a carousel that moves every a timeOut() delay="x" occurs ex: <carousel ajax="foo.bar" file-order="foo.bar;bar.foo;foobar.barfoo" delay="3000" id="thisId" insert="thisId" height="100" width="100" boxes="8" style="height:100;width:800">
-  *  boxes...........= <carousel> attribute to request for x boxes for pictures
-  *  file-order......= ajax to these files, iterating [0,1,2,3]%array.length per call (delimited by ';') ex: <pipe query="key0:value0;" file-order="foo.bar;bar.foo;foobar.barfoo" insert="someID">
-  *  file-index......= counter of which index to use with file-order to go with ajax ex: <pipe ajax="foo.bar" query="key0:value0;" insert="someID">
-  *  incrIndex.......= increment thru index of file-order (0 moves once) (default: 1) ex: <pipe ajax="foo.bar" class="incrIndex" interval="2" file-order="foo.bar;bar.foo;foobar.barfoo" insert="someID">
-  *  decrIndex.......= decrement thru index of file-order (0 moves once) (default: 1) ex: <pipe ajax="foo.bar" class="decrIndex" interval="3" file-order="foo.bar;bar.foo;foobar.barfoo" insert="someID">
-  *  interval........= Take this many steps when stepping through file-order default = 1
-  *  set-attr........= attribute to set in target HTML tag ex: <pipe set-attr="value" ajax="foo.bar" query="key0:value0;" insert="thisOrSomeID">
-  *  mode............= "POST" or "GET" (default: "POST") ex: <pipe mode="POST" set-attr="value" ajax="foo.bar" query="key0:value0;" insert="thisOrSomeID">
-  *  data-pipe.......= name of class for multi-tag data (augment with pipe) *** obfuscated to be reoriented
-  *  multiple........= states that this object has two or more key/value pairs use: states this is a multi-select form box
-  *  remove..........= remove element in tag ex: <anyTag remove="someID;someOtherId;">
-  *  display.........= toggle visible and invisible of anything in the value ex: <anyTag display="someID;someOtherId;">
-  *  json............= returns a JSON file set as value *** obfuscated for now
-  *  callback........= calls function set as attribute value
-  *  headers.........= headers in CSS markup-style-attribute (delimited by '&') <any ajax="foo.bar" headers="foobar:boo&barfoo:barfoo;q:9&" insert="someID">
-  *  form-class......= class name of devoted form elements
-  *  mouse-over......= class name to work thru PipesJS' other attributes on mouseenter/mouseleave
+  *  insert............= return ajax call to this id
+  *  ajax..............= calls and returns the value file's output ex: <pipe ajax="foo.bar" query="key0:value0;" insert="someID">
+  *  query.............= default query string associated with url ex: <anyTag query="key0:value0;key1:value2;" ajax="page.foo">
+  *  <download>........= tag for downloading files ex: <download file="foo.zip" directory="/home/bar/"> (needs ending with slash)
+  *  file..............= filename to download
+  *  directory.........= relative or full path of 'file'
+  *  redirect..........= "follow" the ajax call in POST or GET mode ex: <pipe ajax="foo.bar" redirect query="key0:value0;" insert="someID">
+  *  <lnk>.............= tag for clickable link <lnk ajax="goinghere.html" query="key0:value0;">
+  *  <pipe>............= Tag (initializes on DOMContentLoaded Event) ex: <pipe ajax="foo.bar" query="key0:value0;" insert="someID">
+  *  <dyn>.............= Automatic eventListening tag for onclick="pipes(this)" ex: <dyn ajax="foo.bar" query="key0:value0;" insert="someID">
+  *  <timed>...........= Timed result refreshing tags (Keep up-to-date handling on page) ex: <timed ajax="foo.bar" delay="3000" query="key0:value0;" insert="someID">
+  *  delay.............= delay between <timed> tag refreshes (required for <timed> tag) ex: see <timed>
+  *  <carousel>........= Tag to create a carousel that moves every a timeOut() delay="x" occurs ex: <carousel ajax="foo.bar" file-order="foo.bar;bar.foo;foobar.barfoo" delay="3000" id="thisId" insert="thisId" height="100" width="100" boxes="8" style="height:100;width:800">
+  *  carousel-ajax.....= Class to create Modala pictures for carousel use.
+  *  carousel-auto-off.= Class to stop carousel from moving without buttons
+  *  carousel-vert.....= Class to make carousel vertical, instead of horizontal (default)
+  *  boxes.............= <carousel> attribute to request for x boxes for pictures
+  *  file-order........= ajax to these files, iterating [0,1,2,3]%array.length per call (delimited by ';') ex: <pipe query="key0:value0;" file-order="foo.bar;bar.foo;foobar.barfoo" insert="someID">
+  *  file-index........= counter of which index to use with file-order to go with ajax ex: <pipe ajax="foo.bar" query="key0:value0;" insert="someID">
+  *  incrIndex.........= increment thru index of file-order (0 moves once) (default: 1) ex: <pipe ajax="foo.bar" class="incrIndex" interval="2" file-order="foo.bar;bar.foo;foobar.barfoo" insert="someID">
+  *  decrIndex.........= decrement thru index of file-order (0 moves once) (default: 1) ex: <pipe ajax="foo.bar" class="decrIndex" interval="3" file-order="foo.bar;bar.foo;foobar.barfoo" insert="someID">
+  *  interval..........= Take this many steps when stepping through file-order default = 1
+  *  set-attr..........= attribute to set in target HTML tag ex: <pipe set-attr="value" ajax="foo.bar" query="key0:value0;" insert="thisOrSomeID">
+  *  mode..............= "POST" or "GET" (default: "POST") ex: <pipe mode="POST" set-attr="value" ajax="foo.bar" query="key0:value0;" insert="thisOrSomeID">
+  *  data-pipe.........= name of class for multi-tag data (augment with pipe) *** obfuscated to be reoriented
+  *  multiple..........= states that this object has two or more key/value pairs use: states this is a multi-select form box
+  *  remove............= remove element in tag ex: <anyTag remove="someID;someOtherId;">
+  *  display...........= toggle visible and invisible of anything in the value ex: <anyTag display="someID;someOtherId;">
+  *  json..............= returns a JSON file set as value *** obfuscated for now
+  *  callback..........= calls function set as attribute value
+  *  headers...........= headers in CSS markup-style-attribute (delimited by '&') <any ajax="foo.bar" headers="foobar:boo&barfoo:barfoo;q:9&" insert="someID">
+  *  form-class........= class name of devoted form elements
+  *  mouse-over........= class name to work thru PipesJS' other attributes on mouseenter/mouseleave
   **** ALL HEADERS FOR AJAX are available. They will use defaults to
   **** go on if there is no input to replace them.
   */
@@ -113,7 +116,7 @@ let domContentLoad = (again = false) => {
 // modala(jsonObj,rootNode)
 function modala (value, tempTag, root, id)
 {
-    if (id == true)
+    if (typeof(tempTag) == "string")
     {
         tempTag = document.getElementById(tempTag);
     }
@@ -198,91 +201,66 @@ function fileOrder(elem)
 
 function carousel(elem, auto = true)
 {
-    if (typeof(elem) == "string")
-	elem = document.getElementById(elem);
-    x = document.getElementById(elem.getAttribute("insert"));
-    var mArray = x.getAttribute("file-order").split(";");
-    var y = 1;
-    var crement = 1;
-//    if (elem.hasAttribute("interval"))
-//        crement = parseInt(elem.getAttribute("interval"));
-    if (elem.classList.contains("decrIndex"))
-        crement = (-1);
-    var i = parseInt(x.getAttribute("file-index"));
-    var j = 0;
-    var multiVert = 1;
-    if (x.classList.contains("carousel-vert"))
-    {
-        multiVert = 2;
-    }
-    var n = 0;
-    if (x.classList.contains("carousel-ajax") || elem.classList.contains("carousel-ajax"))
-    {
-        if (x.children.length >= elem.getAttribute("boxes") * multiVert)
-        {
-            i = (crement > 0) ? i + 1 : (i <= 0) ? (mArray.length - 1) : i - 1;
-            var list_ = elem.querySelectorAll('[id^="self_"]');
-            (x.classList.contains("decrIndex")) ? x.removeChild(list_.childFirstElement) : x.removeChild(list_.childLastElement);
+	if (typeof(elem) == "string")
+		elem = document.getElementById(elem);
+	x = document.getElementById(elem.getAttribute("insert"));
+	var mArray = x.getAttribute("file-order").split(";");
+	var y = 1;
+	var crement = 1;
+	if (elem.classList.contains("decrIndex"))
+		crement = (-1);
+	var i = (parseInt(x.getAttribute("file-index"))) ?? 0;
+	var j = 0;
+	var multiVert = 1;
+	if (x.classList.contains("carousel-vert"))
+	{
+		multiVert = 2;
+	}
+	while (x.children.length)
+	{
+		x.removeChild(x.children[0]);
+	}
+	var m = 0;
+	for (n = 0 ; x.children.length < x.getAttribute("boxes") * multiVert ; n++)
+	{
+		if ((x.classList.contains("carousel-ajax") || elem.classList.contains("carousel-ajax")) && x.children.length < elem.getAttribute("boxes"))
+	        {
+	                p = document.createElement("pipe");
+	                p.setAttribute("ajax", mArray[i%mArray.length]);
+	                p.setAttribute("insert", "self_" + n);
+	                p.classList.toggle("modala");
+	                p.id = "self_" + n;
+	                x.appendChild(p);
+	                if (multiVert == 2)
+			{
+		                br = document.createElement("br");
+	                        x.appendChild(br);
+			}
+	                i = (crement > 0) ? i + 1 : (i <= 0) ? (mArray.length - 1) : i - 1;
+			domContentLoad();
+	        }
+		else if ((!x.classList.contains("carousel-ajax") && !elem.classList.contains("carousel-ajax")))
+		{
+			img = document.createElement("img");
+			img.src = mArray[i%mArray.length];
+			x.appendChild(img);
+			i = (crement > 0) ? i + 1 : (i <= 0) ? (mArray.length - 1) : i - 1;
+			br = document.createElement("br");
+			if (multiVert == 2)
+			{
+				n++;
+				x.appendChild(br);
+			}
+		}
+	}
+	while (x.children.length > x.getAttribute("boxes") * multiVert)
+		x.removeChild(x.children[x.children.length-1]);
 
-        }
-        for (m = 0 ; m < x.getAttribute("boxes") * multiVert ; m++) {
-                if (x.children.length < x.getAttribute("boxes") * multiVert)
-                {
-                        p = document.createElement("pipe");
-                        p.setAttribute("ajax", mArray[i%mArray.length]);
-                        p.setAttribute("insert", "self_" + n);
-                        p.classList.toggle("modala");
-                        p.id = "self_" + n;
-                        n++;
-                        (crement < 1) ? x.appendChild(p) : (x.children.length > 1) ? x.insertBefore(p,x.firstChildElement) : x.appendChild(p);
-                        br = document.createElement("br");
-                        if (multiVert == 2)
-                                x.appendChild(br);
-                }
-                else if (x.children.length >= x.getAttribute("boxes") * multiVert)
-                {
-                        if (x.children[Math.abs((j)%x.children.length)].tagName == "BR")
-                                j = (crement > 0) ? j + 1 : (j < 0) ? (x.children.length - 1) : j - 1;
-
-                        x.children[(j%x.children.length)].innerHTML = mArray[(i%mArray.length)];
-
-                        i = (crement > 0) ? i + 1 : (i <= 0) ? (mArray.length - 1) : i - 1;
-                        j = (crement > 0) ? j + 1 : (j <= 0) ? (x.children.length - 1) : j - 1;
-                }
-        }
-        domContentLoad(false);
-        return;
-    }
-    else
-    {
-        for (m = 0 ; m < x.getAttribute("boxes") * multiVert ; m++)
-        {
-            if (x.children.length < x.getAttribute("boxes") * multiVert)
-            {
-                img = document.createElement("img");
-                img.src = mArray[(n)%mArray.length];
-                x.appendChild(img);
-                n++;
-                br = document.createElement("br");
-                if (multiVert == 2)
-                    x.appendChild(br);
-            }
-            else if (x.children.length >= x.getAttribute("boxes") * multiVert)
-            {
-                if (x.children[Math.abs((j)%x.children.length)].tagName == "BR")
-                    j = (crement > 0) ? j + 1 : (j <= 0) ? (x.children.length - 1) : j - 1;
-
-                x.children[Math.abs((j)%x.children.length)].src = imgArray[Math.abs((i)%imgArray.length)];
-                i = (crement > 0) ? i + 1 : (i <= 0) ? (imgArray.length - 1) : i - 1;
-                j  = (crement > 0) ? j + 1 : (j <= 0) ? (x.children.length - 1) : j - 1;
-            }
-        }
-    }
-    var w = (Math.abs(i));
-    x.setAttribute("file-index", w%imgArray.length);
-    var delay = elem.getAttribute("delay");
-    if (!x.classList.contains("carousel-auto-off"))
-    setTimeout(() => { carousel(elem.id, auto); },delay);
+	var w = (Math.abs(i));
+	x.setAttribute("file-index", w%mArray.length);
+	var delay = elem.getAttribute("delay");
+	if (!x.classList.contains("carousel-auto-off"))
+		setTimeout(() => { carousel(elem.id, auto); },delay);
 }
 
 function fileShift(elem)
@@ -380,12 +358,11 @@ function pipes(elem, stop = false) {
     }
     if (elem.hasAttribute("headers"))
     {
-        headers = elem;
-        // var optsArray = elem.getAttribute("headers").split("&");
-        // optsArray.forEach((e,f) => {
-        //     var g = e.split(":");
-        //     headers.set(g[0], g[1]);
-        // });
+        var optsArray = elem.getAttribute("headers").split("&");
+        optsArray.forEach((e,f) => {
+            var g = e.split(":");
+            headers.set(g[0], g[1]);
+        });
     }
     if (elem.hasAttribute("form-class"))
     {
@@ -426,24 +403,25 @@ function pipes(elem, stop = false) {
     navigate(elem, headers, query, formclass);
 }
 
-function setAJAXOpts(elem, opts = null)
+function setAJAXOpts(elem, opts)
 {
 
-    if (elem.hasAttribute("headers"))
-        opts = elem.getAttribute("headers").split("&");
-    opts.forEach((headers) => {
-        var g = headers.split(":");
-        opts.set(g[0], g[1]);
-    });
     // communicate properties of Fetch Request
     var method_thru = (opts["method"] !== undefined) ? opts["method"] : "GET";
     var mode_thru = (opts["mode"] !== undefined) ? opts["mode"]: "no-cors";
+    var cache_thru = (opts["cache"] !== undefined) ? opts["cache"]: "no-cache";
     var cred_thru = (opts["cred"] !== undefined) ? opts["cred"]: '{"Access-Control-Allow-Origin":"*"}';
     // updated "headers" attribute to more friendly "content-type" attribute
     var content_thru = (opts["content-type"] !== undefined) ? opts["content-type"]: '{"Content-Type":"text/html"}';
+    var redirect_thru = (opts["redirect"] !== undefined) ? opts["redirect"]: "manual";
+    var refer_thru = (opts["referrer"] !== undefined) ? opts["referrer"]: "referrer";
     opts.set("method", method_thru); // *GET, POST, PUT, DELETE, etc.
     opts.set("mode", mode_thru); // no-cors, cors, *same-origin
+    opts.set("cache", cache_thru); // *default, no-cache, reload, force-cache, only-if-cached
     opts.set("credentials", cred_thru); // include, same-origin, *omit
+    opts.set("content-type", content_thru); // content-type UPDATED**
+    opts.set("redirect", redirect_thru); // manual, *follow, error
+    opts.set("referrer", refer_thru); // no-referrer, *client
     opts.set('body', JSON.stringify(content_thru));
 
     return opts;
@@ -487,6 +465,8 @@ function navigate(elem, opts = null, query = "", classname = "")
 
     var rawFile = new XMLHttpRequest();
     rawFile.open(opts.get("method"), elem_qstring, true);
+    console.log(elem);
+
     if (elem.classList.contains("json"))
     {
         rawFile.onreadystatechange = function() {
@@ -535,8 +515,8 @@ function navigate(elem, opts = null, query = "", classname = "")
             if (rawFile.readyState === 4) {
                 var allText = ""; // JSON.parse(rawFile.responseText);
                 allText = JSON.parse(rawFile.responseText);
+		console.log(allText);
                 var x = document.getElementById(elem.getAttribute("insert"));
-                x.innerHTML = "";
                 modala(allText, x);
                 if (elem.hasAttribute("callback"))
                 {
